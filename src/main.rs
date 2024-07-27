ae8bc83833e49dd3f344dbcb398493e458f24b76
Mårten Blankfors <marten@blankfors.se>
2024-07-27 13:28:23 +0200

    wip: Main function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-fn main() {
-    n2p::tui::init_terminal().expect("failed to init terminal");
+#[tokio::main]
+async fn main() {
+    let mut terminal = n2p::tui::init_terminal().expect("failed to init terminal");
+
+    let mut app = n2p::app::App::new();
+
+    app.run(&mut terminal).await.expect("failed to run app");
 
     n2p::tui::restore_terminal().expect("failed to restore terminal");
     println!("Hello, world!");
